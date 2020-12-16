fa7811a9b2312e6d1c240837f4b397467cd8e2f8
Thiago Gaspar <thiagogasparr@gmail.com>
2020-12-16 18:34:03 -0300

    Finalizando desafio 3-1

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
-server.get('/', (req, res) => {
-    return res.send('Olá')
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", { 
+    express: server
 })
 
+
+server.get("/", (req, res) => {
+    return res.render("index")
+})
+
+server.get("/courses", (req, res) => {
+    return res.render("courses")
+})
+
+server.get("/about", (req, res) => {
+    return res.render("about")
+})
+
+server.use((req, res) => {
+    res.status(404).render("not-found");
+})
 
 server.listen(5000, function () {
     console.log('server is running')
